7b56301c580559865626a3577bd04a3c1c652911
Anne M. Archibald <peridot.faceted@gmail.com>
2009-10-08 11:24:03 -0400

    Documentation.

--- poly.py
+++ poly.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 class Polynomial:
+    """Class for representing polynomials.
+
+    This class represents a polynomial as a linear combination of
+    basis functions. The basis functions 1, x, x**2, ... are perhaps
+    the most familiar set of basis functions, but they can lead to
+    very severe numerical problems. So this class keeps track of
+    the set of basis functions used to represent this polynomial. Most
+    calculations are actually delegated to the class representing the 
+    basis. Calculations between polynomials represented in different 
+    bases require explicit conversion, since conversion between bases
+    is often a numerically unstable operation.
+    """
 
     def __init__(self, basis, coefficients):
         self.basis = basis
@@ -39,9 +51,53 @@ class Polynomial:
         return self + (-1.)*other
 
 
+class Basis:
+    """Abstract base class for polynomial bases.
 
-class PowerBasis:
+    This class exists primarily to make explicit the interface
+    that a polynomial basis must support.
+    """
+
+    def __init__(self):
+        pass
+
+    def extend(self, coefficients, n):
+        """Extend a coefficient list to length n.
+
+        A polynomial of degree m is naturally represented by a vector
+        of coefficients of length m. But it is often useful to be able
+        to extend the vector of coefficients to length n; for the power
+        basis, this just amounts to padding with zeros, but for other
+        bases this may be a nontrivial operation.
+        """
+        raise NotImplementedError
+
+    def evaluate(self, coefficients, x):
+        """Evaluate a polynomial at x.
+
+        It should be possible for x to be an array of arbitrary shape; the
+        result will have the same shape.
+        """
+        raise NotImplementedError
+
+    def multiply(self, coefficients, other_coefficients):
+        """Compute the coefficients of the product."""
+        raise NotImplementedError
+
+    def __eq__(self, other):
+        """Test for equality."""
+        raise NotImplementedError
+    def __ne__(self, other):
+        return not self==other
+    def __hash__(self):
+        raise NotImplementedError
+
+
+class PowerBasis(Basis):
+    """The basis 1, x, x**2, ..."""
+
     def __init__(self,center=0):
+        Basis.__init__(self)
         self.center = center
 
     def extend(self, coefficients, n):
@@ -72,8 +128,6 @@ class PowerBasis:
 
     def __eq__(self, other):
         return isinstance(other, PowerBasis) and self.center == other.center
-    def __ne__(self, other):
-        return not self==other
     def __hash__(self):
         return hash(self.center) # Include self's type?
 
@@ -100,17 +154,46 @@ def bit_reverse(n):
     return r, denom
 
 def chebyshev_points_sequence(n):
+    """The extrema of the Chebyshev polynomials, in a subrandom order."""
+    # FIXME: Make sure we're using the right points.
     m, denom = bit_reverse(n)
     return np.cos(np.pi*m/float(denom))
 
 class LagrangeBasis:
+    """This class represents a polynomial by its values at specified points.
+
+    This is the natural representation when constructing the Lagrange
+    interpolating polynomial, for example.
+
+    The points chosen can be specified upon construction of a basis object.
+    When further points are needed, for example when multiplying polynomials,
+    the points are chosen to be Chebyhev points, which cluster at the edges
+    of the given interval in a way that is optimal in terms of numerical 
+    stability.
+
+    This class is technically mutable, since it keeps track of which points 
+    have been specified, but the modifications are invisible to users. The
+    class is not currently thread-safe; it is probably best to have a separate
+    copy per thread.
+    """
 
     def __init__(self, initial_points=[], interval=None):
+        """Construct a basis.
+
+        A set of initial points to include in the basis can be specified.
+        If interval is specified, then polynomials in this basis will 
+        be fairly well-behaved on that interval. If it is not specified,
+        it will use the range spanned by initial_points, if these are 
+        available, or (-1,1) if they are not.
+        """
+        Basis.__init__(self)
         self.initial_points = tuple(initial_points)
         self.points = np.array(initial_points, dtype=np.float)
         if interval is None:
-            if initial_points:
+            if len(initial_points)>1:
                 self.interval = np.amax(self.points), np.amin(self.points)
+            elif len(initial_points)==1:
+                self.interval = (initial_points[0]-1,initial_points[0]+1)
             else:
                 self.interval = (-1,1)
         else:
@@ -120,12 +203,15 @@ class LagrangeBasis:
 
     def __eq__(self, other):
         return isinstance(other, LagrangeBasis) and self.initial_points == other.initial_points
-    def __ne__(self, other):
-        return not self==other
     def __hash__(self):
         return hash(tuple(self.initial_points)) # Do arrays hash?
 
     def extend(self, coefficients, n):
+        """Extend a coefficient array.
+
+        This function does two things: select new sample points if necessary,
+        and evaluate the polynomial specified by coefficients at more points.
+        """
         a, b = self.interval
         new_points = []
         while n>len(self.points)+len(new_points):
@@ -147,6 +233,13 @@ class LagrangeBasis:
         return new_coefficients
 
     def evaluate(self, coefficients, x):
+        """Evaluate a polynomial at a new point.
+
+        This code uses barycentric interpolation, which is quite stable, 
+        numerically. The weights are currently not cached between calls, 
+        nor does the code use the analytic expression for the weights in
+        the case that the points are Chebyshev points.
+        """
         if not np.isscalar(x):
             x = np.asarray(x)
             r = np.zeros(x.shape)
@@ -168,6 +261,11 @@ class LagrangeBasis:
         return np.sum(wx*coefficients)/np.sum(wx)
 
     def multiply(self, coefficients, other_coefficients):
+        """Multiply two polynomials.
+
+        This code simply extends the polynomials to a high enough order
+        then does pointwise multiplication.
+        """
         if len(coefficients)==0 or len(other_coefficients)==0:
             return np.zeros(0)
         l = len(coefficients)+len(other_coefficients)-1
