6b6aba9c4295d68374fd613431a41bdeec2df1d9
Anne Archibald <peridot@octopus.(none)>
2009-10-08 08:42:08 -0400

    Basic operations

--- poly.py
+++ poly.py
@@ -1,119 +1,46 @@
+import numpy as np
 
+class PowerBasisPolynomial:
 
-class Poly:
+    def __init__(self, coefficients):
+        self.coefficients = np.array(coefficients)
 
-    def __init__(self,coefficients,basis):
-        self.coefficients = coefficients
-        self.basis = basis
-
-    def change_basis(self,new_basis):
-        pass
-
-
-class PolynomialBasis:
-
-    def __init__(self):
-        pass
-
-    def evaluate(self, n, x):
-        raise NotImplementedError
-
-    def evaluate_series(self, coefficients, x):
-        r = np.zeros(np.shape(x))
-        for (n,c) in enumerate(coefficients):
-            r += c*self.evaluate(n,x)
+    def __call__(self, x):
+        r = np.zeros_like(x)
+        if len(self.coefficients)==0:
+            return r
+        r += self.coefficients[-1]
+        for c in self.coefficients[-2::-1]:
+            r *= x
+            r += c
         return r
 
-    def derivative(self, n, der=1):
-        if der==0:
-            r = np.zeros((n+1,))
-            r[-1] = 1
-            return r
-        elif der==1:
-            raise NotImplementedError
-        elif der>1:
-            return derivative_series(self.derivative(n,der=1),der=der-1)
+    def __add__(self, other):
+        if isinstance(other,PowerBasisPolynomial):
+            l = max(len(self.coefficients),len(other.coefficients))
+            c = np.zeros(l)
+            c[:len(self.coefficients)] = self.coefficients
+            c[:len(other.coefficients)] += other.coefficients
+            return PowerBasisPolynomial(c)
         else:
-            raise ValueError("derivative method requires a nonnegative integer order (got %g)" % der)
+            c = np.copy(self.coefficients)
+            c[0] += other
+            return PowerBasisPolynomial(c)
 
-    def derivative_series(self, coefficients, der=1):
-        if der==0:
-            return coefficients
-        elif der==1:
-            r = None
-            for n,c in reverse(enumerate(coefficients)):
-                v = c*self.derivative(n)
-                if r is None:
-                    r = v
-                else:
-                    r[:n] += v
-            return r
-        elif der>1 and der==int(der):
-            for i in range(der):
-                coefficients = self.derivative_series(coefficients, der=1)
+    def __mul__(self, other):
+        if isinstance(other,PowerBasisPolynomial):
+            c = np.zeros(len(self.coefficients)+len(other.coefficients))
+            for i, ci in enumerate(self.coefficients):
+                c[i:i+len(other.coefficients)] += ci*other.coefficients
         else:
-            raise ValueError("derivative_series method requires a nonnegative integer order (got %g)" % der)
+            c = np.copy(self.coefficients)
+            c *= other
+            return PowerBasisPolynomial(c)
+    def __rmul__(self, other):
+        return self*other
+    def __sub__(self, other):
+        return self + (-1)*other
 
-    def convert(self, coefficients, current_basis):
-        raise NotImplementedError
 
-class PowerBasis:
-    def __init__(self, center=0.):
-        self.center = center
 
-    def evaluate(self, n, x):
-        return (x-self.center)**n
 
-    def derivative(self, n, der=1):
-        if der==1:
-            r = np.zeros(n-1)
-            r[-1] = n
-            return r
-        else:
-            return PowerBasis.derivative(self, n, der)
-
-    def convert(self, coefficients, current_basis):
-        r = np.zeros(len(coefficients))
-        fac = 1
-        for i in range(len(coefficients)):
-            r[i] = current_basis.evaluate_series(coefficients,self.center)/fac
-            coefficients = current_basis.derivative_series(coefficients)
-            fac *= i+1
-        return r
-
-def OrthogonalPolynomialBasis(PolynomialBasis):
-    
-    def __init__(self, an_func, sqrt_bn_func, mu, limits=(-np.inf,np.inf)):
-        PolynomialBasis.__init__(self)
-        self.an_func = an_func
-        self.sqrt_bn_func = sqrt_bn_func
-        self.mu = mu
-
-    def roots(self, n):
-        x, w = scipy.special.orthogonal.gen_roots_and_weights(n, self.an_func, self.sqrt_bn_func, self.mu)
-        return x
-
-    def weights(self, n):
-        x, w = scipy.special.orthogonal.gen_roots_and_weights(n, self.an_func, self.sqrt_bn_func, self.mu)
-        return w
-
-    def convert(self, coefficients, current_basis):
-        pass
-
-def orthogonal_fit(orthogonal_basis, n, func):
-    """Find the polynomial of degree n that best fits func
-
-    This fit is computed rapidly using the orthogonality relations in
-    the given polynomial basis.
-    """
-    pass
-
-def ChebyshevBasis(OrthogonalPolynomialBasis):
-
-    def __init__(self):
-        OrthogonalPolynomialBasis.__init__(self)
-
-    def evaluate(self, n, x):
-        return np.cos(n*np.arccos(x))
-
-    
